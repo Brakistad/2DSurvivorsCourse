830b91dca193bcd040f0c1b49cc2c4238c3d7ea1
brakistad <brakistad@gmail.com>
2024-02-04 22:25:46 +0100

    Add new files and update existing files

--- scenes/ui/pause_menu.gd
+++ scenes/ui/pause_menu.gd
@@ -9,57 +9,57 @@ var options_menu_scene = preload("res://scenes/ui/options_menu.tscn")
 var is_closing = false
 
 func _ready():
-    get_tree().paused = true
-    resume_button.pressed.connect(on_resume_button_pressed)
-    options_button.pressed.connect(on_options_button_pressed)
-    quit_button.pressed.connect(on_quit_button_pressed)
+	get_tree().paused = true
+	resume_button.pressed.connect(on_resume_button_pressed)
+	options_button.pressed.connect(on_options_button_pressed)
+	quit_button.pressed.connect(on_quit_button_pressed)
 
-    $AnimationPlayer.play("default")
-    
-    panel_container.pivot_offset = panel_container.size / 2
+	$AnimationPlayer.play("default")
+	
+	panel_container.pivot_offset = panel_container.size / 2
 
-    var tween = create_tween()
-    tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
-    tween.tween_property(panel_container, "scale", Vector2.ONE, .3)\
-    .set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, .3)\
+	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 
 func _unhandled_input(event):
-    if event.is_action_pressed("pause"):
-        close()
-        get_tree().root.set_input_as_handled()
+	if event.is_action_pressed("pause"):
+		close()
+		get_tree().root.set_input_as_handled()
 
 func close():
-    if is_closing:
-        return
-    is_closing = true
+	if is_closing:
+		return
+	is_closing = true
 
-    $AnimationPlayer.play_backwards("default")
+	$AnimationPlayer.play_backwards("default")
 
-    var tween = create_tween()
-    tween.tween_property(panel_container, "scale", Vector2.ONE, 0)
-    tween.tween_property(panel_container, "scale", Vector2.ZERO, .3)\
-    .set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
-    
-    await tween.finished
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ONE, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, .3)\
+	.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
+	
+	await tween.finished
 
-    get_tree().paused = false
-    queue_free()
+	get_tree().paused = false
+	queue_free()
 
 func on_resume_button_pressed():
-    close()
+	close()
 
 func on_options_button_pressed():
-    ScreenTransition.transition()
-    await ScreenTransition.transitioned_halfway
-    var options_menu_instance = options_menu_scene.instantiate()
-    add_child(options_menu_instance)
-    options_menu_instance.back_pressed.connect(on_options_menu_back_pressed.bind(options_menu_instance))
+	ScreenTransition.transition()
+	await ScreenTransition.transitioned_halfway
+	var options_menu_instance = options_menu_scene.instantiate()
+	add_child(options_menu_instance)
+	options_menu_instance.back_pressed.connect(on_options_menu_back_pressed.bind(options_menu_instance))
 
 func on_quit_button_pressed():
-    ScreenTransition.transition()
-    await ScreenTransition.transitioned_halfway
-    get_tree().paused = false
-    get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
+	ScreenTransition.transition()
+	await ScreenTransition.transitioned_halfway
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
 
 func on_options_menu_back_pressed(options_menu: Node):
-    options_menu.queue_free()
+	options_menu.queue_free()
