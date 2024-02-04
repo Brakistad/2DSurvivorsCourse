8a3888c189d847d1295f272057e54a3eb4131ac2
brakistad <brakistad@gmail.com>
2024-02-04 17:49:01 +0100

    Refactor screen transition code

--- scenes/autoload/screen_transition.gd
+++ scenes/autoload/screen_transition.gd
@@ -3,12 +3,16 @@ extends CanvasLayer
 signal transitioned_halfway
  
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var color_rect: ColorRect = $ColorRect
  
 func transition():
-	$ColorRect.visible = true
+	(color_rect.material as ShaderMaterial).set_shader_parameter("percent", 0.0)
+	color_rect.visible = true
 	animation_player.play("default")
 	await animation_player.animation_finished
+	(color_rect.material as ShaderMaterial).set_shader_parameter("percent", 1.0)
 	transitioned_halfway.emit()
 	animation_player.play_backwards("default")
 	await animation_player.animation_finished
-	$ColorRect.visible = false
+	color_rect.visible = false
+	(color_rect.material as ShaderMaterial).set_shader_parameter("percent", 0.0)
