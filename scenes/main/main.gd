cb78107abfb3c383355af68ca8d08613873c6980
brakistad <brakistad@gmail.com>
2024-02-04 02:18:16 +0100

    Add pause menu functionality

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -2,10 +2,18 @@ extends Node
 
 @export var end_screen_scene: PackedScene
 
+var pause_menu_scene: PackedScene = preload("res://scenes/ui/pause_menu.tscn")
 
 func _ready():
 	($%Player as Player).health_component.died.connect(on_player_died)
 	
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		var pause_menu_instance = pause_menu_scene.instantiate()
+		add_child(pause_menu_instance)
+		get_tree().root.set_input_as_handled()
+
 
 func on_player_died():
 	var end_screen_instance = end_screen_scene.instantiate() as EndScreen
