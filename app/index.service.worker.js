ec509790455823cab3d7a82a0cde9902045f234b
brakistad <brakistad@gmail.com>
2024-02-06 01:43:36 +0100

    Add COI service worker and include it in index.html

--- app/index.service.worker.js
+++ app/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1707179350|7922716300";
+const CACHE_VERSION = "1707180125|8697891374";
 const CACHE_PREFIX = "2DSurvivorsCours-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
